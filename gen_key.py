a80a14ecb476604c339db047fec17fffaaa08a37
Matt Fonner <matt@matthewfonner.com>
2019-09-20 21:34:42 -0400

    Added Python3 check to gen_key()

--- gen_key.py
+++ gen_key.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 from cryptography.fernet import Fernet
+import sys
+
+# Check that we are using Python 3 to run
+try:
+    if sys.version_info[0] < 3:
+        raise Exception("This script requires Python 3")
+except Exception:
+    print("This script requires Python 3")
+    sys.exit()
 
 key = Fernet.generate_key()
 
